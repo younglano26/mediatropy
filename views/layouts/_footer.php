efeb489aa7b303dbb6a5801558c4e9cf5987c3f3
younglano26 <younglano2009@gmail.com>
2015-06-21 12:25:34 +0100

    Create _footer.php

---
+++ views/layouts/_footer.php
@@ -0,0 +1,26 @@
+<footer class="footer">
+    <div class="container center-block">
+        <div class='row'>
+            <div class='col-md-8'>
+                <p class="address">
+                    12 Marina View, Asia Square Tower 2&nbsp;·&nbsp;Singapore, ,018961&nbsp;·&nbsp;Singapore<br/>
+                    · Phone: (65) 6922 6888 &nbsp;&nbsp;&nbsp;&nbsp;· Fax: (65) 6922 6899 <br/>
+                    In Singapore, call: 1800-325-9595 
+                </p>
+            </div>
+            <div class="col-md-4">
+                <div id="social-icons" class="pull-right nav-item" > 
+                    <a href="https://www.facebook.com/thewestinsingapore"><img src="img/icons/fb-icon.png"></a>
+                    &nbsp;<a href="https://twitter.com/westinsingapore"><img src="img/icons/twitter-icon.png"></a>
+                    &nbsp;<a href="https://foursquare.com/v/the-westin-singapore/51c2c98f8bbda6c871fa0bcc"><img src="img/icons/foursquare-icon.png"></a>
+                    &nbsp;<a href="https://plus.google.com/104254873148666855553/about"><img src="img/icons/google-plus-icon.png"></a>
+                    &nbsp;<a href="https://instagram.com/thewestinsingapore/"><img src="img/icons/instagram-icon.png"></a>
+                    <a href="https://www.youtube.com/channel/UCx5vARuzJDqX0jBL9GZzwyA"><img src="img/icons/youtube-icon.png"></a>
+                </div> 
+            </div>
+            </div>
+        </div>
+        
+    
+        
+</footer>
